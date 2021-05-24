859e1d4102eb571861276274854a22a7003c2430
wellysonvie <wellysonvie@gmail.com>
2021-05-24 12:42:07 -0300

    chore: initial project setup

---
+++ src/pages/index.jsx
@@ -0,0 +1,19 @@
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+
+      </header>
+
+      <main className={styles.main}>
+
+      </main>
+
+      <footer className={styles.footer}>
+
+      </footer>
+    </div>
+  )
+}
