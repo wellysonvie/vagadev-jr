2d5613b6218989252e1921998c3ede0b37b4bd25
wellysonvie <wellysonvie@gmail.com>
2021-05-25 17:38:34 -0300

    feat: navbar implementation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
+import NavBar from './NavBar';
 import styles from '../styles/components/Header.module.scss';
 
+
 const Header = () => {
+
+  const [showNavBar, setShowNavBar] = useState(false);
+
   return (
     <header className={styles.header}>
       <div className={styles.brand}>
-        <img src="/assets/svg/icon_hamburguer.svg" alt="Menu" />
+        <img
+          src="/assets/svg/icon_hamburguer.svg"
+          alt="Menu"
+          onClick={() => setShowNavBar(!showNavBar)}
+        />
         <img src="/assets/svg/logo_n1_rush.svg" alt="N1 Rush" />
       </div>
+      {showNavBar && <NavBar />}
       <ul className={styles.menuOptions}>
         <li>
           <img src="/assets/svg/paper-plane.svg" alt="Contato" />
