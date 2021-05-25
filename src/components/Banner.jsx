b2c4fb5eb84d209a5b3aac810ef743d541f22949
wellysonvie <wellysonvie@gmail.com>
2021-05-25 15:22:01 -0300

    feat: transition of slides in banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,30 +1,66 @@
+import { useEffect, useRef, useState } from 'react';
 import styles from '../styles/components/Banner.module.scss';
 
-const Banner = () => {
+const Banner = ({ slides }) => {
+
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  const timeoutRef = useRef();
+
+  function nextSlide() {
+    if (currentSlideIndex === slides.length - 1) {
+      setCurrentSlideIndex(0);
+    } else {
+      setCurrentSlideIndex(currentSlideIndex + 1);
+    }
+  }
+
+  function previousSlide() {
+    if (currentSlideIndex === 0) {
+      setCurrentSlideIndex(slides.length - 1);
+    } else {
+      setCurrentSlideIndex(currentSlideIndex - 1)
+    }
+  }
+
+  useEffect(() => {
+    timeoutRef.current = setInterval(() => nextSlide(), 1000 * 10);
+    return () => clearTimeout(timeoutRef.current);
+  }, [nextSlide]);
+
   return (
     <div className={styles.banner}>
       <div
         className={styles.bannerBackground}
-        style={{ backgroundImage: "url('/assets/img/principal_banner_desktop.jpg')" }}
+        style={{ backgroundImage: `url('${slides[currentSlideIndex].images.desktop}')` }}
       />
       <div className={styles.bannerInfo}>
-        <h2>MORTAL KOMBAT</h2>
+        <h2>{slides[currentSlideIndex].name}</h2>
         <div className={styles.price}>
-          <h3>R$ 299</h3>
-          <span>,99</span>
+          <h3>{slides[currentSlideIndex].price.split(',')[0]}</h3>
+          <span>{slides[currentSlideIndex].price.split(',')[1]}</span>
         </div>
-        <p>Mortal Kombat X combina uma apresentação cinemática única com uma jogabilidade totalmente nova. Os jogadores podem escolher pela primeira vez diversas variantes de cada personagem, afetando tanto a estratégia como o estilo de luta.</p>
+        <p>{slides[currentSlideIndex].description}</p>
       </div>
       <div className={styles.bannerSlider}>
         <div className={styles.currentProduct}>
-          <h4>MORTAL KOMBAT</h4>
+          <h4>{slides[currentSlideIndex].name}</h4>
           <span></span>
         </div>
         <div className={styles.controller}>
-          <span>1/2</span>
+          <span>{currentSlideIndex + 1}/{slides.length}</span>
           <div>
-            <img src="/assets/svg/angle-left-solid.svg" alt="Anterior" title="Anterior" />
-            <img src="/assets/svg/angle-right-solid.svg" alt="Próximo" title="Próximo" />
+            <img
+              src="/assets/svg/angle-left-solid.svg"
+              alt="Anterior"
+              title="Anterior"
+              onClick={previousSlide}
+            />
+            <img
+              src="/assets/svg/angle-right-solid.svg"
+              alt="Próximo"
+              title="Próximo"
+              onClick={nextSlide}
+            />
           </div>
         </div>
       </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,13 @@ import Banner from '../components/Banner'
 import Header from '../components/Header'
 import styles from '../styles/Home.module.scss'
 
-export default function Home() {
+export default function Home({ bannerSlides }) {
   return (
     <div className={styles.container}>
       <Header />
 
       <main className={styles.main}>
-        <Banner />
+        <Banner slides={bannerSlides} />
       </main>
 
       <footer className={styles.footer}>
@@ -16,4 +16,38 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      bannerSlides: [
+        {
+          images: {
+            desktop: '/assets/img/principal_banner_desktop.jpg',
+            mobile: '/assets/img/principal_banner_mobile.jpg'
+          },
+          name: 'MORTAL KOMBAT',
+          price: 'R$ 299,99',
+          description: `Mortal Kombat X combina uma apresentação cinemática única 
+            com uma jogabilidade totalmente nova. Os jogadores podem escolher pela 
+            primeira vez diversas variantes de cada personagem, afetando tanto a 
+            estratégia como o estilo de luta.`
+        },
+        {
+          images: {
+            desktop: '/assets/img/principal_banner_desktop_02.jpg',
+            mobile: '/assets/img/principal_banner_mobile_02.jpg'
+          },
+          name: 'RED DEAD REDEMPTION 2',
+          price: 'R$ 299,99',
+          description: `Arthur Morgan e a gangue Van der Linde são bandidos em fuga. 
+            Com agentes federais e os melhores caçadores de recompensas no seu encalço, 
+            a gangue precisa roubar, assaltar e lutar para sobreviver no impiedoso coração 
+            dos Estados Unidos. Conforme divisões internas profundas ameaçam despedaçar a gangue, 
+            Arthur deve fazer uma escolha entre os seus próprios ideais e a lealdade à gangue que o criou.`
+        },
+      ]
+    }
+  }
 }
